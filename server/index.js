b76eada1bdb56ef6b13e236485d4cca8ed5630ca
Rajesh Sharma <sharma.rajesh2k1@gmail.com>
2026-06-12 02:32:20 +0530

    port update

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const UserRoutes = require("./routes/authRoutes.js")
+const path = require("path")
 
 dotenv.config();
 
@@ -16,20 +16,14 @@ app.use(cors({
   credentials: true
 }));
 
-
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req,res) => {
-    res.send("LemonCart backend is working properly")
-});
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/payment', require('./routes/paymentRoutes'));
 app.use('/api/analytics', require('./routes/analyticsRoutes'));
+
 
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
