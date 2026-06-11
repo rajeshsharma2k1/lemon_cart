f7b00cbdc74179623b7d4c18a259283fd776eb67
Rajesh Sharma <sharma.rajesh2k1@gmail.com>
2026-06-12 02:14:39 +0530

    Production

--- server/index.js
+++ server/index.js
@@ -31,6 +31,19 @@ app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/payment', require('./routes/paymentRoutes'));
 app.use('/api/analytics', require('./routes/analyticsRoutes'));
 
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('LemonCart API is running in Development mode...');
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
